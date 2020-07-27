6ed46bb4adf3e1a6a54d742a6221bf8371d9e3b2
mmg.prokazyuk.o@gmail.com <mmg.prokazyuk.o@gmail.com>
2020-07-27 23:12:58 +0300

    index.html

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,17 @@
 import React, {Component, Suspense} from 'react';
 import Navbar from './components/Navbar/Navbar'
 import './App.css';
-import {Route} from "react-router-dom";
+import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import connect from "react-redux/lib/connect/connect";
-import {withRouter} from "react-router-dom";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import {Provider} from "react-redux";
+import store from "./redux/redux-store";
 
 
 const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
@@ -50,6 +51,20 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 });
 
-export default compose(
+const AppContainer = compose(
     withRouter,
-    connect(mapStateToProps, {initializeApp}))(App);
+    connect(mapStateToProps, {initializeApp})
+)(App);
+
+const SocialNetworkApp = (props) => {
+    return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer />
+        </Provider>
+    </BrowserRouter>
+};
+
+export default SocialNetworkApp;
+// export default compose(
+//     withRouter,
+//     connect(mapStateToProps, {initializeApp}))(App);
